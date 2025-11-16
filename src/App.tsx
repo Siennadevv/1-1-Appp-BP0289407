bb83a983378b662414f07451f7b7e1a287f2fb8f
Siennadevv <siennadevv@gmail.com>
2025-11-16 15:38:05 +0000

    fix: small update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Sienna</h1>
+      <h1>Sienna + Sienna</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
